a2dbc0acf8a730df106ab14994084b3a4c0e2b32
Ellen Lee <lunacy20@gmail.com>
2017-02-03 03:07:33 +0800

    re-arrange element

--- wp-content/themes/inkness-child/page.php
+++ wp-content/themes/inkness-child/page.php
@@ -29,7 +29,7 @@ get_header(); ?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
+<br>
 <?php get_sidebar(); ?>
 <?php get_sidebar('footer'); ?>
 <?php get_footer(); ?>
--- wp-content/themes/inkness-child/single-grass_course.php
+++ wp-content/themes/inkness-child/single-grass_course.php
@@ -75,6 +75,8 @@ get_header(); ?>
 								<?php if( $course_price ){ ?>
 									<div class="course-price price-big">
 										<h2>$<?php echo $course_price; ?></h2>
+										<br>
+										<a href="#enroll-area" class="btn btn-warning btn-lg">立刻報名</a>
 									</div>
 								<?php } ?>
 							</div>
@@ -118,7 +120,7 @@ get_header(); ?>
 										if( $course_arrangment ){
 											echo "
 												<div>
-													<h3>授課方式</h3>
+													<h3>授課時間</h3>
 													<p>$course_arrangment</p>
 												</div>
 											";
@@ -137,84 +139,87 @@ get_header(); ?>
 						</div>
 					</div>
 					<hr>
+					<div id="teacher_and_schedule">
+						<!-- 講師資訊 -->
+						<?php if( $teacher_name ){ ?>
+							<div id="grass_teacher">
+								<div class="teacher-mainframe">
+									<?php
+									  foreach ( $teacher_name as $key => $value ) {
 
-					<!-- 講師資訊 -->
-					<?php if( $teacher_name ){ ?>
-						<div id="grass_teacher">
-							<div class="teacher-mainframe">
-								<?php
-								  foreach ( $teacher_name as $key => $value ) {
+								  	// The Query
+										$grass_teacher_param = array(
+											'post_type' => 'grass_teacher',
+											'title' => $value,
+											'posts_count' => '1',
+										);
 
-							  	// The Query
-									$grass_teacher_param = array(
-										'post_type' => 'grass_teacher',
-										'title' => $value,
-										'posts_count' => '1',
-									);
+										$the_query = new WP_Query( $grass_teacher_param );
 
-									$the_query = new WP_Query( $grass_teacher_param );
-
-									if ( $the_query->have_posts() ) : while( $the_query->have_posts() ) : ($the_query->the_post());
-								?>
+										if ( $the_query->have_posts() ) : while( $the_query->have_posts() ) : ($the_query->the_post());
+									?>
 
 
-									<a class="teacher-box flex-space-center" href="<?php the_permalink(); ?>">
-										<?php if( has_post_thumbnail() ){ ?>
-											<div class="teacher-avatar-xs"><?php the_post_thumbnail(); ?>
-										</div>
-										<?php } ?>
-										<div class="teacher-info">
-											<h4 class="teacher-name"><?php echo the_title(); ?></h4>
-											<?php echo the_excerpt(); ?>
-										</div>
-									</a>
+										<a class="teacher-box flex-space-center" href="<?php the_permalink(); ?>">
+											<?php if( has_post_thumbnail() ){ ?>
+												<div class="teacher-avatar-xs"><?php the_post_thumbnail(); ?>
+											</div>
+											<?php } ?>
+											<div class="teacher-info">
+												<h4 class="teacher-name"><?php echo the_title(); ?></h4>
+												<?php echo the_excerpt(); ?>
+											</div>
+										</a>
 
-								<?php
-										endwhile;
+									<?php
+											endwhile;
 
-									elseif( $teacher_name ): ?>
-										<div class="teacher-box">
-											<h4><?php echo $value; ?></h4>
-										</div>
-								<?php
-									endif; wp_reset_postdata(); } ?>
+										elseif( $teacher_name ): ?>
+											<div class="teacher-box">
+												<h4><?php echo $value; ?></h4>
+											</div>
+									<?php
+										endif; wp_reset_postdata(); } ?>
+								</div>
 							</div>
-						</div>
-					<?php	} ?><!-- 講師資訊結束 -->
-					<hr>
-					<?php if( $class_num > 0 ): ?>
-						<div id="course-schedule">
-							<div id="course-schedule-mainframe">
-								<?php
-									foreach ( $class_info as $key => $value ) {
-								  	$the_info = explode( ",", $value );
-								?>
-									<div class="class-box">
-										<div class="h1"><?php echo $key+1; ?></div>
-										<div class="class-info">
-											<h5 class="class-date"><?php echo $the_info[1]; ?></h5>
-											<h4><?php echo $the_info[2]; ?></h4>
-											<p class="class-content">
-												<?php
-													if( count($the_info) > 4 ){
-														for($i = 3; $i <= count($the_info); $i++){
-														  echo $the_info[$i]."<br>";
-													  }
-													} else{
-														echo $the_info[3];
-													}
-												?>
-											</p>
+						<?php	} ?><!-- 講師資訊結束 -->
+						<?php if( $class_num > 0 ): ?>
+							<div id="course-schedule">
+								<div id="course-schedule-mainframe">
+									<?php
+										foreach ( $class_info as $key => $value ) {
+									  	$the_info = explode( ",", $value );
+									?>
+										<div class="class-box">
+											<div class="h1"><?php echo $key+1; ?></div>
+											<div class="class-info">
+												<h5 class="class-date"><?php echo $the_info[1]; ?></h5>
+												<h4><?php echo $the_info[2]; ?></h4>
+												<p class="class-content">
+													<?php
+														if( count($the_info) > 4 ){
+															for($i = 3; $i <= count($the_info); $i++){
+															  echo $the_info[$i]."<br>";
+														  }
+														} else{
+															echo $the_info[3];
+														}
+													?>
+												</p>
+											</div>
 										</div>
-									</div>
-							  <?php } ?>
-							</div>
-						</div><!-- .entry-content -->
-					<?php endif; ?>
+								  <?php } ?>
+								</div>
+							</div><!-- .entry-content -->
+						<?php endif; ?>
+					</div><!-- 課程資訊結束 -->
 				</div>
+
+
 	<div id="enroll-area" class="flex-center">
 		<div class="enroll-area-frame">
-			<h3>立刻報名</h3>
+			<h3><br><br>立刻報名<br><small><?php the_title(); ?></small></h3>
+
 			<?php echo do_shortcode('[contact-form-7 id="75" title="form-course-enrollment" html_class="form-course-enrollment"]'); ?>
 		</div>
 	</div>
